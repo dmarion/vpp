320c1de4ff0dc79496b3b6d68f998f50244b5157
Damjan Marion <damarion@cisco.com>
2021-11-28 22:57:15 +0100

    vppinfra: toeplitz hash
    Type: feature
    Change-Id: I0ccf280c4b769ee624cfb780146b847a0e8e4942
    Signed-off-by: Damjan Marion <damarion@cisco.com>

--- src/vppinfra/vector/test/test.h
+++ src/vppinfra/vector/test/test.h
@@ -6,14 +6,31 @@
 #define included_test_test_h
 
 #include <vppinfra/cpu.h>
+#ifdef __linux__
+#include <sys/ioctl.h>
+#include <linux/perf_event.h>
+#endif
 
 typedef clib_error_t *(test_fn_t) (clib_error_t *);
+
+struct test_perf_;
+typedef void (test_perf_fn_t) (int fd, struct test_perf_ *tp);
+
+typedef struct test_perf_
+{
+  u64 n_ops;
+  char *op_name;
+  char *name;
+  test_perf_fn_t *fn;
+} test_perf_t;
 
 typedef struct test_registration_
 {
   char *name;
   u8 multiarch : 1;
   test_fn_t *fn;
+  test_perf_t *perf_tests;
+  u32 n_perf_tests;
   struct test_registration_ *next;
 } test_registration_t;
 
@@ -31,5 +48,40 @@ extern test_registration_t *test_registrations[CLIB_MARCH_TYPE_N_VARIANTS];
     test_registrations[CLIB_MARCH_SFX (CLIB_MARCH_VARIANT_TYPE)] = r;         \
   }                                                                           \
   test_registration_t CLIB_MARCH_SFX (__test_##x)
+
+#define TEST_PERF_TESTS(...)                                                  \
+  (test_perf_t[])                                                             \
+  {                                                                           \
+    __VA_ARGS__, {}                                                           \
+  }
+
+static_always_inline void
+perf_event_ioctl (int fd, u32 req)
+{
+#ifdef __x86_64__
+  asm inline("syscall"
+	     :
+	     : "D"(fd), "S"(req), "a"(__NR_ioctl), "d"(PERF_IOC_FLAG_GROUP)
+	     : "rcx", "r11" /* registers modified by kernel */);
+#else
+  ioctl (fd, req, PERF_IOC_FLAG_GROUP);
+#endif
+}
+
+static_always_inline void
+perf_event_reset (int fd)
+{
+  perf_event_ioctl (fd, PERF_EVENT_IOC_RESET);
+}
+static_always_inline void
+perf_event_enable (int fd)
+{
+  perf_event_ioctl (fd, PERF_EVENT_IOC_ENABLE);
+}
+static_always_inline void
+perf_event_disable (int fd)
+{
+  perf_event_ioctl (fd, PERF_EVENT_IOC_DISABLE);
+}
 
 #endif
